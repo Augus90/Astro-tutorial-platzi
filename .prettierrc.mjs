4de425b07969cca1041c8a0ce4d9704d5c797e44
Augus90 <augusto_buchino@hotmail.com>
2024-02-05 23:27:04 -0300

    [UPDATE] config

---
+++ .prettierrc.mjs
@@ -0,0 +1,20 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: true,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "**/*astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+export default config;
